fcffa4e7fd49abd4c3fe2cc09b65eb44deb29226
jash-desai <jash.13.desai@gmail.com>
2023-11-18 11:51:06 +0530

    minor chngaes.

--- lib/meta/pages/splash/splash.page.dart
+++ lib/meta/pages/splash/splash.page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, deprecated_member_use
+// ignore_for_file: deprecated_member_use
 
 import 'dart:async';
 import 'package:portfolio/app/shared/colors.dart';
--- lib/meta/sections/projects/projects.section.dart
+++ lib/meta/sections/projects/projects.section.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:portfolio/app/shared/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
